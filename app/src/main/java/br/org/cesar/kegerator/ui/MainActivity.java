6b213d4f551fc7ed424a739c7d7ddbb46457b72e
aam <35492766+aammelo@users.noreply.github.com>
2018-01-16 09:24:38 -0400

    Initial commit
    initial commit
    
    initial commit

---
+++ app/src/main/java/br/org/cesar/kegerator/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package br.org.cesar.kegerator.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.org.cesar.kegerator.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/br/org/cesar/kegerator/utils/Constants.java
@@ -0,0 +1,4 @@
+package br.org.cesar.kegerator.utils;
+
+public class Constants {
+}
